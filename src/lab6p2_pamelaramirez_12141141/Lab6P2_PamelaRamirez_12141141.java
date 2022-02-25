34d04943e4b7f536fb6186145fb35b8d8a825bfa
pamelagiselle8 <pamelaramirez@unitec.edu>
2022-02-25 13:20:20 -0600

    Subir proyecto

---
+++ src/lab6p2_pamelaramirez_12141141/Lab6P2_PamelaRamirez_12141141.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6p2_pamelaramirez_12141141;
+
+/**
+ *
+ * @author pame
+ */
+public class Lab6P2_PamelaRamirez_12141141 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
